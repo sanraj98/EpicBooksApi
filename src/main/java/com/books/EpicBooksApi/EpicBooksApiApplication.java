f6f1b2a8591fee0ecea6a1577980192c25db10fd
santhosh raj <santhoshrajrathinam@gmail.com>
2024-06-23 19:08:13 +0530

    add prj epicbooksapis

---
+++ src/main/java/com/books/EpicBooksApi/EpicBooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.books.EpicBooksApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicBooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicBooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books/EpicBooksApi/EpicBooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.EpicBooksApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicBooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
