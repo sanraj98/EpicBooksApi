a5b3dd284ada5a15d8b9d2b6665dd6a3ffedf9ec
unknown <santhoshrajrathinam@gmail.com>
2024-06-27 19:01:01 +0530

    changes on 27-06-2024

---
+++ src/main/java/com/books/EpicBooksApi/Config/CrossConfig.java
@@ -0,0 +1,25 @@
+package com.books.EpicBooksApi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Configuration
+public class CrossConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
